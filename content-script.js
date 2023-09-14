ad6e0b48ebb48f9ebf738d15b5b711fc4788d6e6
Roman Pronskiy <roman@pronskiy.com>
2023-09-14 20:39:53 +0200

    Init Chrome extension for easier ChatGPT use

---
+++ content-script.js
@@ -0,0 +1,57 @@
+console.log('content-script.js');
+
+const PROMPTS = {
+    proofread: `Proofread: 
+    
+$CLIPBOARD$`,
+}
+
+String.prototype.mapReplace = function(map) {
+    var regex = [];
+    for(var key in map)
+        regex.push(key.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"));
+    return this.replace(new RegExp(regex.join('|'),"g"),function(word){
+        return map[word];
+    });
+};
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        console.log(message)
+        if (message.prompt === undefined || PROMPTS[message.prompt] === undefined) {
+            return;
+        }
+
+        setTimeout(async () => {
+            addPromptOnPage(await renderPrompt(message.prompt))
+        }, 1000);
+    }
+)
+
+function addPromptOnPage(prompt) {
+    const prompt_textarea = document.getElementById("prompt-textarea")
+    setNativeValue(prompt_textarea, prompt)
+    prompt_textarea.dispatchEvent(new Event('input', { bubbles: true }))
+    prompt_textarea.nextSibling.click()
+}
+
+async function renderPrompt(prompt) {
+    const text = await navigator.clipboard.readText();
+    return PROMPTS[prompt].mapReplace({
+        '$CLIPBOARD$': text,
+    })
+}
+
+function setNativeValue(element, value){
+    const {set: valueSetter} = Object.getOwnPropertyDescriptor(element, 'value') || {}
+    const prototype = Object.getPrototypeOf(element)
+    const {set: prototypeValueSetter} = Object.getOwnPropertyDescriptor(prototype, 'value') || {}
+
+    if (prototypeValueSetter && valueSetter !== prototypeValueSetter) {
+        prototypeValueSetter.call(element, value)
+    } else if (valueSetter) {
+        valueSetter.call(element, value)
+    } else {
+        throw new Error('The given element does not have a value setter')
+    }
+}
---
+++ worker.js
@@ -0,0 +1,18 @@
+chrome.commands.onCommand.addListener(async command => {
+  if (command === "new-tab") {
+    const win = await chrome.windows.getCurrent();
+    await chrome.windows.update(win.id, { focused: true });
+    const new_tab = await chrome.tabs.create({
+        url: 'https://chat.openai.com/'
+    });
+    await sleep(1000);
+    await chrome.tabs.sendMessage(
+        new_tab.id, 
+        {prompt: 'proofread'},
+    )
+  }
+});
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
